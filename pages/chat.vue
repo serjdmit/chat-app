435ee6560ff620572db870cba8c5f610b5ce9b77
Sergei Dmitrijev <mr.dmit@gmail.com>
2019-12-25 01:16:51 +0200

    Added forms, layout and pages

---
+++ pages/chat.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>CHAT PAGE {{ user.name }}</h1>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  middleware: ['chat'],
+  computed: mapState(['user']),
+  head() {
+    return {
+      title: `Room ${this.user.room}`
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,54 @@
 <template>
   <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <v-btn @click="message">
-        Message
-      </v-btn>
+    <v-flex xs12 sm8>
+      <v-card min-width="400">
+        <v-card-title><h1>Nuxt chat</h1></v-card-title>
+        <v-card-text>
+          <v-form ref="form" v-model="valid" lazy-validation>
+            <v-text-field
+              v-model="name"
+              :counter="16"
+              :rules="nameRules"
+              label="Your name"
+              required
+            ></v-text-field>
+
+            <v-text-field
+              v-model="room"
+              :rules="roomRules"
+              label="Room name"
+              required
+            ></v-text-field>
+            <v-btn
+              :disabled="!valid"
+              @click="submit"
+              color="success"
+              class="mr-4"
+            >
+              Come in
+            </v-btn>
+          </v-form>
+        </v-card-text>
+      </v-card>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
+import { mapMutations } from 'vuex'
 export default {
+  data: () => ({
+    valid: true,
+    name: '',
+    nameRules: [
+      (v) => !!v || 'Name is required',
+      (v) => (v && v.length <= 16) || 'Name must be less than 16 characters'
+    ],
+    room: '',
+    roomRules: [(v) => !!v || 'Room name is required']
+  }),
+  layout: 'empty',
+  head: { title: 'Welcome to Nuxt chat' },
   sockets: {
     connect() {
       // eslint-disable-next-line
@@ -17,10 +56,17 @@ export default {
     }
   },
   methods: {
-    message() {
-      this.$socket.emit('createMessage', {
-        text: 'some random message'
-      })
+    ...mapMutations(['setUser']),
+    submit() {
+      if (this.$refs.form.validate()) {
+        const user = {
+          name: this.name,
+          room: this.room
+        }
+
+        this.setUser(user)
+        this.$router.push('/chat')
+      }
     }
   }
 }
